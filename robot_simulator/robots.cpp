e0f7ec0dd7c062c56b5f08adbb5b4b8d3e222d01
Savant Krishna <savant.2020@gmail.com>
2012-08-29 04:44:12 +0530

    Task 2 : Added robot::set() and robot::move() functions

--- robot_simulator/robots.cpp
+++ robot_simulator/robots.cpp
@@ -26,6 +26,44 @@ class robot {
 
 };
 
+void robot::set(float nx, float ny, float nor)//sets the position and orientation to given values
+{
+    x=nx;
+    y=ny;
+    orientation=nor;
+}
+
+robot robot::move(float steering, float distance, float tol=0.0001, float max_steering=M_PI_4)//float tol=0.0001, float max_steering=M_PI_4)
+{
+  float turn,center_x,center_y,radius;
+  //Some checks to do : if distance is positive, steering angle is not high
+    if (steering>max_steering){
+        steering=max_steering;
+    }
+    if(steering<-max_steering){
+        steering=-max_steering;
+    }
+    if(distance<0.0){
+        distance=0.0;
+    }
+    robot newrobot=robot();
+    //set steering noise here if there or distance noise
+    turn=tan(steering);
+    if (abs(turn)<tol){
+        newrobot.x=x+ distance*cos(orientation);
+        newrobot.y=y+ distance*sin(orientation);
+        newrobot.orientation= fmod(orientation+turn,float(2.0*M_PI));
+    }else{
+        radius=distance/turn;
+        center_x=x-sin(orientation)*radius;
+        center_y=y+cos(orientation)*radius;
+        newrobot.orientation= fmod(orientation+turn,float(2.0*M_PI));
+        newrobot.x=center_x+sin(orientation)*radius;
+        newrobot.y=center_y-cos(orientation)*radius;
+    }
+    return newrobot;
+}
+
 int main(){
     cout<<M_PI<<endl;
 
